f4c9d94d292710f34ab94ecc2cacffcfa94c10cc
腰立辉 <yaolh@yonyou.com>
2020-02-24 14:25:41 +0800

    调整目录结构

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png">    
 	<router-link class="ys1" to="/a">转向A组件</router-link>
 	<router-link class="ys1" to="/b">转向B组件</router-link>
 	<router-view></router-view>
@@ -9,13 +8,5 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
