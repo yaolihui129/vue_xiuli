564f18e47153a87f12fc28c5e2f4ace6b4ad9922
腰立辉 <yaolh@yonyou.com>
2020-03-06 16:47:11 +0800

    更新配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer:{
+	port:8081,
+	proxy:{
+		'/mock':{
+			target:'http://127.0.0.1:9061',
+			ws:false,
+			//将主机头的原点改为UR
+			changeOrigin:false
+		}
+	}
+  }
 }
